008d86983b8827576e56547cc589f3cf2f1c8097
Will Miao <13051207myq@gmail.com>
2025-03-24 03:46:12 +0800

    Update workflow

--- __init__.py
+++ __init__.py
@@ -2,13 +2,13 @@ from .py.lora_manager import LoraManager
 from .py.nodes.lora_loader import LoraManagerLoader
 from .py.nodes.trigger_word_toggle import TriggerWordToggle
 from .py.nodes.lora_stacker import LoraStacker
-from .py.nodes.save_image import SaveImage
+# from .py.nodes.save_image import SaveImage
 
 NODE_CLASS_MAPPINGS = {
     LoraManagerLoader.NAME: LoraManagerLoader,
     TriggerWordToggle.NAME: TriggerWordToggle,
     LoraStacker.NAME: LoraStacker,
-    SaveImage.NAME: SaveImage
+    # SaveImage.NAME: SaveImage
 }
 
 WEB_DIRECTORY = "./web/comfyui"
